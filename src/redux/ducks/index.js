7245ceaf15aae651e777ccc511d9e5271d77e399
Phong <phong940253@gmail.com>
2021-09-17 18:59:38 +0700

    add change login state

--- src/redux/ducks/index.js
+++ src/redux/ducks/index.js
@@ -13,6 +13,7 @@ const initialStateUser = {
 export const CHANGE_EMAIL = "CHANGE_EMAIL";
 export const CHANGE_NAME = "CHANGE_NAME";
 export const CHANGE_UID = "CHANGE_UID";
+export const CHANGE_LOGIN = "CHANGE_LOGIN";
 
 export const change_email = (email) => ({
     type: CHANGE_EMAIL,
@@ -29,6 +30,11 @@ export const change_uid = (uid) => ({
     info: uid,
 });
 
+export const change_login = (login) => ({
+    type: CHANGE_LOGIN,
+    info: login,
+});
+
 export const userReducer = (state = initialStateUser, action) => {
     switch (action.type) {
         case CHANGE_EMAIL:
@@ -37,6 +43,8 @@ export const userReducer = (state = initialStateUser, action) => {
             return { ...state, name: action.info };
         case CHANGE_UID:
             return { ...state, uid: action.info };
+        case CHANGE_LOGIN:
+            return { ...state, login: action.info };
         default:
             return state;
     }
